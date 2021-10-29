6b93520b94a90330df0d6fd3e382f495c3c0b96e
Dakkshesh <dakkshesh5@gmail.com>
2021-10-29 19:11:30 +0530

    kprofiles: add `fb_notifier` support for auto kprofiles
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -6,7 +6,11 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kprofiles.h>
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
 #include <linux/msm_drm_notify.h>
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+#include <linux/fb.h>
+#endif
 
 static bool screen_on = true;
 static unsigned int mode = 0;
@@ -15,6 +19,7 @@ module_param(mode, uint, 0664);
 static int msm_drm_notifier_callback(struct notifier_block *self,
 				unsigned long event, void *data)
 {
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
 	struct msm_drm_notifier *evdata = data;
 	int *blank;
 
@@ -39,6 +44,29 @@ static int msm_drm_notifier_callback(struct notifier_block *self,
 		mode = 0;
 		break;
 	}
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+	struct fb_event *evdata = data;
+	int *blank;
+
+	if (event != FB_EVENT_BLANK)
+		goto out;
+
+	blank = evdata->data;
+	switch (*blank) {
+	case FB_BLANK_POWERDOWN:
+		if (!screen_on)
+			break;
+		screen_on = false;
+		mode = 1;
+		break;
+	case FB_BLANK_UNBLANK:
+		if (screen_on)
+			break;
+		screen_on = true;
+		mode = 0;
+		break;
+	}
+#endif
 
 out:
 	return NOTIFY_OK;
@@ -63,15 +91,18 @@ inline unsigned int active_mode(void)
     }
 }
 
-static struct notifier_block fb_notifier_block = {
+static struct notifier_block msm_drm_notifier_block = {
 	.notifier_call = msm_drm_notifier_callback,
 };
 
 static int  __init kprofiles_notifier_init(void)
 {
 
-	msm_drm_register_client(&fb_notifier_block);
-
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+	msm_drm_register_client(&msm_drm_notifier_block);
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+	fb_register_client(&msm_drm_notifier_block);
+#endif
 	return 0;
 }
 
