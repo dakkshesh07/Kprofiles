69634e728916277fbb090143bb2dcc26707ddb54
Dakkshesh <dakkshesh5@gmail.com>
2021-11-14 22:07:28 +0530

    kprofiles: assign `set_mode` during init
    Currently kprofiles the common_notifier_callback() assigns mode the value which is assigned to set_mode,
    
    But, during boot up set_mode is never assigned to anything thus leading to profile being set to 0 during bootup.
    
    To fix this, assign set_mode the value which is being assigned to mode by default, so that common_notifier_callback() can use it to set it properly
    
    Acked-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -118,10 +118,11 @@ inline unsigned int active_mode(void)
 static struct notifier_block common_notifier_block = {
 	.notifier_call = common_notifier_callback,
 };
+#endif
 
-static int  __init kprofiles_notifier_init(void)
+static int  __init kprofiles_init(void)
 {
-
+	set_mode = mode;
 #ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
 	msm_drm_register_client(&common_notifier_block);
 #elif defined(CONFIG_AUTO_KPROFILES_FB)
@@ -130,8 +131,7 @@ static int  __init kprofiles_notifier_init(void)
 	return 0;
 }
 
-late_initcall(kprofiles_notifier_init);
-#endif
+late_initcall(kprofiles_init);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dakkshesh");
 MODULE_DESCRIPTION("KernelSpace Profiles");
