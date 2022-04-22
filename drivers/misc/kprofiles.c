630fd1dce67c8188f49ad08d9d21b9e1de3243f4
Cyber Knight <cyberknight755@gmail.com>
2022-04-22 09:25:22 +0800

    Kprofiles: Update copyright
    - Include my copyright.
    - Update copyright to 2023.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2021 Dakkshesh <dakkshesh5@gmail.com>.
+ * Copyright (C) 2021-2023 Dakkshesh <dakkshesh5@gmail.com>.
+ * Copyright (C) 2021-2023 Cyber Knight <cyberknight755@gmail.com>.
  */
 
 #include <linux/module.h>
