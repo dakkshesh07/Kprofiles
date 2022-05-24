c31b45958d7c7992d87ef9a68090553097b47354
Dakkshesh <dakkshesh5@gmail.com>
2022-05-24 18:31:01 +0530

    kp: main: simplify `active_mode()`
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- main.c
+++ main.c
@@ -11,24 +11,13 @@ static int mode = 0;
 module_param(mode, uint, 0664);
 
 
-int active_mode(void) {
+int active_mode(void)
+{
+	if (mode < 4)
+		return mode;
 
-	if (mode == 1) {
-		return 1;
-	}
-
-	if (mode == 2) {
-		return 2;
-	}
-
-	if (mode == 3) {
-		return 3;
-	}
-
-	else {
-		pr_info("Invalid value passed, falling back to level 0\n");
-		return 0;
-	}
+	pr_info("Invalid value passed, falling back to level 0\n");
+	return 0;
 }
 
 MODULE_LICENSE("GPL v2");
