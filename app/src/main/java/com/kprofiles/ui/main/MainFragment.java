8d66d66a1d8a978cf80864dcbcc35487cf786ea2
Martin Valba <kailing2@live.com>
2022-06-03 21:53:15 +0300

    Main application functionality changes + theme side changes
    * Strings pulled from libsu/Shell now are processed to remove []
    * When app is launched from stopped state then app pull's now node results to keep track
    * Some code side cleanup

--- app/src/main/java/com/kprofiles/ui/main/MainFragment.java
+++ app/src/main/java/com/kprofiles/ui/main/MainFragment.java
@@ -9,7 +9,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.fragment.app.Fragment;
 
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,12 +21,14 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.kprofiles.R;
 import com.topjohnwu.superuser.Shell;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MainFragment extends Fragment {
 
-    private Context context;
     private Activity activity;
     private SharedPreferences sharedpreferences;
-    private int pos;
 
     // Exported node path's
     private static final String KP_AUTO_NODE = "/sys/module/kprofiles/parameters/auto_kprofiles";
@@ -52,7 +53,6 @@ public class MainFragment extends Fragment {
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.context = getContext();
         this.activity = getActivity();
 
         // Prompt user to get su
@@ -73,16 +73,24 @@ public class MainFragment extends Fragment {
         final AppCompatSpinner kprofile_mode = promptView.findViewById(R.id.kprofile_mode);
         final MaterialButton mode_apply = promptView.findViewById(R.id.mode_apply);
 
+        // Load modes from nodes and save them with sharedpref
+        //// KProfile auto node
+        List<String> node_kp_auto_result = new ArrayList<>();
+        Shell.cmd("cat " + KP_AUTO_NODE).to(node_kp_auto_result).exec();
+        String node_kp_auto_result_new = ShellStringProcess(node_kp_auto_result);
+        sharedpreferences.edit().remove("kp_auto").apply();
+        sharedpreferences.edit().putString("kp_auto", String.valueOf(node_kp_auto_result_new)).apply();
 
+        //// KProfile mode node
+        List<String> node_kp_node_result = new ArrayList<>();
+        Shell.cmd("cat " + KP_MODE_NODE).to(node_kp_node_result).exec();
+        String node_kp_node_result_new = ShellStringProcess(node_kp_node_result);
+        sharedpreferences.edit().remove("kp_mode").apply();
+        sharedpreferences.edit().putString("kp_mode", String.valueOf(node_kp_node_result_new)).apply();
 
         // Check weather sharedpref's have values and set them as they are. Otherwise disable everything to stock
         //// KP Auto ( doesn't reapply actual function but just moves the switch where user defined it to be )
-        final boolean kpauto = sharedpreferences.getBoolean("kp_auto", false);
-        if (kpauto == true) {
-            kprofile_auto.setChecked(true);
-        } else {
-            kprofile_auto.setChecked(false);
-        }
+        kprofile_auto.setChecked(Objects.equals(sharedpreferences.getString("kp_auto", "Y"), "Y"));
 
         //// KP Mode
         if (sharedpreferences.getString("kp_mode", "None").equals("None")) {
@@ -102,13 +110,13 @@ public class MainFragment extends Fragment {
             if (isChecked) {
                 Toast.makeText(getActivity(),
                         "Auto KP enabled", Toast.LENGTH_SHORT).show();
-                sharedpreferences.edit().putBoolean("kp_auto", true).apply();
+                sharedpreferences.edit().putString("kp_auto", "Y").apply();
                 Shell.cmd("echo " + AUTO_ENABLE + " > " + KP_AUTO_NODE).exec();
 
             } else {
                 Toast.makeText(getActivity(),
                         "Auto KP disabled", Toast.LENGTH_SHORT).show();
-                sharedpreferences.edit().putBoolean("kp_auto", false).apply();
+                sharedpreferences.edit().putString("kp_auto", "N").apply();
                 Shell.cmd("echo " + AUTO_DISABLE + " > " + KP_AUTO_NODE).exec();
             }
         });
@@ -120,27 +128,21 @@ public class MainFragment extends Fragment {
                 if (kprofile_mode.getSelectedItem().toString().equals("None")) {
                     sharedpreferences.edit().remove("kp_mode").apply();
                     sharedpreferences.edit().putString("kp_mode", "None").apply();
-                    return;
                 } else if (kprofile_mode.getSelectedItem().toString().equals("Disabled")) {
                     sharedpreferences.edit().remove("kp_mode").apply();
                     sharedpreferences.edit().putString("kp_mode", "Disabled").apply();
-                    return;
                 } else if (kprofile_mode.getSelectedItem().toString().equals("Battery")) {
                     sharedpreferences.edit().remove("kp_mode").apply();
                     sharedpreferences.edit().putString("kp_mode", "Battery").apply();
-                    return;
                 } else if (kprofile_mode.getSelectedItem().toString().equals("Balanced")) {
                     sharedpreferences.edit().remove("kp_mode").apply();
                     sharedpreferences.edit().putString("kp_mode", "Balanced").apply();
-                    return;
                 } else if (kprofile_mode.getSelectedItem().toString().equals("Performance")) {
                     sharedpreferences.edit().remove("kp_mode").apply();
                     sharedpreferences.edit().putString("kp_mode", "Performance").apply();
-                    return;
                 } else {
                     Toast.makeText(getActivity(),
                             R.string.kp_performance, Toast.LENGTH_SHORT).show();
-                    return;
                 }
             }
             @Override
@@ -151,38 +153,31 @@ public class MainFragment extends Fragment {
         });
 
         // KProfile mode ( apply ) also make sure that selected value matches before apply
-        mode_apply.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (sharedpreferences.getString("kp_mode", "None").equals("None")) {
-                    Log.d("This is:", "None");
-                } else if (sharedpreferences.getString("kp_mode", "Disabled").equals("Disabled")) {
-                    Log.d("This is:", "Disabled");
-                    Shell.cmd("echo " + DISABLED + " > " + KP_MODE_NODE).exec();
-                } else if (sharedpreferences.getString("kp_mode", "Battery").equals("Battery")) {
-                    Log.d("This is:", "Battery");
-                    Shell.cmd("echo 0 " + BATTERY + " > " + KP_MODE_NODE).exec();
-                } else if (sharedpreferences.getString("kp_mode", "Balanced").equals("Balanced")) {
-                    Log.d("This is:", "Balanced");
-                    Shell.cmd("echo " + BALANCED + " > " + KP_MODE_NODE).exec();
-                } else if (sharedpreferences.getString("kp_mode", "Performance").equals("Performance")) {
-                    Log.d("This is:", "Performance");
-                    Shell.cmd("echo " + PERFORMANCE + " > " + KP_MODE_NODE).exec();
-                }
+        mode_apply.setOnClickListener(v -> {
+            if (sharedpreferences.getString("kp_mode", "None").equals("None")) {
+                Log.d("This is:", "None");
+            } else if (sharedpreferences.getString("kp_mode", "Disabled").equals("Disabled")) {
+                Log.d("This is:", "Disabled");
+                Shell.cmd("echo " + DISABLED + " > " + KP_MODE_NODE).exec();
+            } else if (sharedpreferences.getString("kp_mode", "Battery").equals("Battery")) {
+                Log.d("This is:", "Battery");
+                Shell.cmd("echo 0 " + BATTERY + " > " + KP_MODE_NODE).exec();
+            } else if (sharedpreferences.getString("kp_mode", "Balanced").equals("Balanced")) {
+                Log.d("This is:", "Balanced");
+                Shell.cmd("echo " + BALANCED + " > " + KP_MODE_NODE).exec();
+            } else if (sharedpreferences.getString("kp_mode", "Performance").equals("Performance")) {
+                Log.d("This is:", "Performance");
+                Shell.cmd("echo " + PERFORMANCE + " > " + KP_MODE_NODE).exec();
             }
         });
 
         return promptView;
     }
 
-    public static void saveString(Context context, String key, String value) {
-        SharedPreferences sharedPrefs = PreferenceManager
-                .getDefaultSharedPreferences(context);
-        sharedPrefs.edit().putString(key, value).apply();
-    }
-
-    public static String getString(Context context, String key, String defaultValue) {
-        SharedPreferences sharedPrefs = PreferenceManager
-                .getDefaultSharedPreferences(context);
-        return sharedPrefs.getString(key, defaultValue);
+    // Here we just replace dumb [ and ] of libsu output from ( Load modes from nodes ) comment
+    public String ShellStringProcess(List<String> process) {
+        String string = process.toString();
+        String string1 = string.replace("[", "");
+        return string1.replace("]", "");
     }
 }
