e72e68e806fcb5f8571573cf4e026dbe5422be41
Dakkshesh <dakkshesh5@gmail.com>
2023-07-16 22:54:18 +0530

    kp: Update code comments to better match Linux coding style
    Also add a small description about the module on top.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2021-2023 Dakkshesh <dakkshesh5@gmail.com>.
+ * KernelSpace Profiles
+ *
+ * This Linux kernel module provides a framework for managing and switching between system profiles or modes
+ * at the kernel level. Each profile represents a specific configuration of kernel features and settings
+ * optimized for different use cases such as battery life, balanced performance, or maximum performance.
+ *
+ * The module supports various subsystems, including MSM DRM, MI DRM, and framebuffer (FB). It integrates
+ * with these subsystems to receive notifications about screen state changes and adjust the active profile
+ * accordingly.
+ *
+ * The module offers functions for setting the profile mode, overriding the mode temporarily, and retrieving
+ * the active profile mode. Profiles can be dynamically switched based on system events, user requests, or
+ * time-based rules.
+ *
+ * For more information and usage examples, refer to the README file at:
+ * https://github.com/dakkshesh07/Kprofiles/blob/main/README.md
+ *
+ * Copyright (C) 2021-2023 Dakkshesh <dakkshesh5@gmail.com>
+ * Version: 5.0.3
+ * License: GPL-2.0
  */
+
 
 #include <linux/delay.h>
 #include <linux/module.h>
@@ -47,12 +67,16 @@ module_param(kp_mode, int, 0664);
 
 DEFINE_MUTEX(kplock);
 
-/*
- * This function can be used to change profile to any given mode
- * for a specific period of time during any in-kernel event,
- * then return to the previously active mode.
+/**
+ * kp_set_mode_rollback - Change profile to a given mode for a specific duration
+ * @level: The profile mode level to set (0-3)
+ * @duration_ms: The duration to keep the profile mode in milliseconds
  *
- * usage example: kp_set_mode_rollback(3, 55)
+ * This function changes the profile to the specified mode for a specific
+ * duration during any in-kernel event, and then returns to the previously
+ * active mode.
+ *
+ * Usage example: kp_set_mode_rollback(3, 55);
  */
 void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 {
@@ -79,11 +103,14 @@ void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 }
 EXPORT_SYMBOL(kp_set_mode_rollback);
 
-/*
- * This function can be used to change profile to
- * any given mode during any in-kernel event.
+/**
+ * kp_set_mode - Change profile to a given mode
+ * @level: The profile mode level to set (0-3)
  *
- * usage example: kp_set_mode(3)
+ * This function changes the profile to the specified mode during any
+ * in-kernel event.
+ *
+ * Usage example: kp_set_mode(3);
  */
 void kp_set_mode(unsigned int level)
 {
@@ -105,28 +132,32 @@ void kp_set_mode(unsigned int level)
 }
 EXPORT_SYMBOL(kp_set_mode);
 
-/*
- * This function returns a number from 0 and 3 depending on the profile
- * selected. The former can be used in conditions to disable/enable
- * or tune kernel features according to a profile mode.
+/**
+ * kp_active_mode - Get the currently active profile mode
  *
- * usage exmaple:
+ * This function returns a number from 0 to 3 depending on the active profile mode.
+ * The returned value can be used in conditions to disable/enable or tune kernel
+ * features according to the profile mode.
+ *
+ * Return:
+ * The currently active profile mode (0-3)
+ *
+ * Usage example:
  *
  * switch (kp_active_mode()) {
  * case 1:
- *	things to be done when battery profile is active
- *	break;
+ *     // Things to be done when battery profile is active
+ *     break;
  * case 2:
- *	things to be done when balanced profile is active
- * 	break;
+ *     // Things to be done when balanced profile is active
+ *     break;
  * case 3:
- *	things to be done when performance profile is active
- *	break;
+ *     // Things to be done when performance profile is active
+ *     break;
  * default:
- *	things to be done when kprofiles is disabled
- *	break;
+ *     // Things to be done when kprofiles is disabled
+ *     break;
  * }
- *
  */
 int kp_active_mode(void)
 {
@@ -227,8 +258,7 @@ static int __init kp_init(void)
 	if (ret)
 		pr_err("Failed to register notifier, err: %d\n", ret);
 
-	pr_info("Kprofiles " KPROFILES_VERSION
-		" loaded successfully. For further details, visit https://github.com/dakkshesh07/Kprofiles/blob/main/README.md\n");
+	pr_info("Kprofiles " KPROFILES_VERSION " loaded successfully. For further details, visit https://github.com/dakkshesh07/Kprofiles/blob/main/README.md\n");
 	pr_info("Copyright (C) 2021-2023 Dakkshesh <dakkshesh5@gmail.com>.\n");
 
 	return ret;
